888d6bf82e769696299e4fcc4e799a3bbbb19473
Даниил Тертень <daniil.terten@monopoly.su>
2023-01-27 11:09:55 +0300

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzClass
+    {
+        public static List<string> ToStringsFizzBuzz(List<int> numbers)
+        {
+            var strings = new List<string>();
+            foreach (var num in numbers)
+            {
+                var addString = num switch
+                {
+                    var value when value % 3 == 0 => "Fizz",
+                    var value when value % 5 == 0 => "Buzz",
+                    var value when value % 15 == 0 => "FizzBuzz",
+                    _ => num.ToString()
+                };
+                strings.Add(addString);
+            }
+
+            return strings;
+        }
+    }
+}
---
+++ FizzBuzzTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FizzBuzz;
+using FluentAssertions;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestFizzBuzz()
+        {
+            var expectedStrings = new List<string>() { "Fizz", "4" };
+            var result = FizzBuzzClass.ToStringsFizzBuzz(new List<int>() { 3, 4});
+            result.Should().BeEquivalentTo(expectedStrings);
+        }
+    }
+}
